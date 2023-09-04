8938cf8f509732b06b58c0f96b12f684567a7d57
Shafiqul Islam <shafiqswh@gmail.com>
2023-09-03 23:16:31 -0500

    first commit

---
+++ src/main/java/edu/mum/cs/cs425/demowebaps/eregistrar/eregistrar/controller/HomeController.java
@@ -0,0 +1,14 @@
+package edu.mum.cs.cs425.demowebaps.eregistrar.eregistrar.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value = {"/", "/eregister","/eregister/home"})
+    public String displayHomePage(){
+        return "home/index";
+    }
+}
